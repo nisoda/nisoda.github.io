fda48c3098a5c44d1a958bcc7cbbd4cd4de7b3a0
Nicole isoda <nisoda@hawaii.edu>
2015-09-16 16:51:26 -1000

    Connected routes.js to application. My first SPA! :heart_eyes:

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Created by Naoki on 9/15/2015.
  */
 
-var app = angular.module('nisoda.github.io', []);
+var app = angular.module('nisoda.github.io', ['ui.router', 'Routes']);
 
 app.controller('MainController', ['$scope', function($scope) {
     $scope.hello = "Welcome!";
